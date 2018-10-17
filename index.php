88fb1507ab885f29d1bf64d3e19d0acd07fcd684
KsenyiaK <42876825+KsenyiaK@users.noreply.github.com>
2018-10-17 23:41:07 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+﻿<?php
+    include "fileLoad.php";
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Homework 4</title>
+    <style>
+        .gallery{
+            width: 900px;
+            margin: 0 auto;
+            display: flex;
+            flex-wrap: wrap;
+            background-color: lightgray;
+            border: 1px solid #000;
+
+        }
+        .item{
+            margin: 10px;
+        }
+        .item img:hover{
+            filter: saturate(10);
+            transition: filter 500ms;
+        }
+    </style>
+</head>
+<body>
+<br><br>
+<form action="?" method="post" enctype="multipart/form-data">
+    <label>Размер не должен превышать 3mb<input type="file" name="imges"></label>
+    <input type="submit" value="Загрузить картинку">
+</form>
+
+<?php
+$bigImgFolder = "img/max/";
+$smallImgFolder = "img/min/";
+
+if(isset($_FILES['imges'])) {
+    uploadFile($bigImgFolder, $smallImgFolder);
+}
+?>
+
+<div class="gallery">
+<?php
+    outputImagesFromDir($bigImgFolder, $smallImgFolder);
+?>
+</div>
+
+</body>
+</html>
